940a0c549e3a971bf1dc85ddc353fc6e294ca871
Isaac Torbett <izacht13@gmail.com>
2017-02-05 20:21:44 -0500

    PIP support

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycml',
+    version="0.0.1",
+    url='http://github.com/izacht13/pycml/',
+    license='MIT',
+    author='Isaac Torbett',
+    author_email='izacht13@gmail.com',
+    description='An interpreter for CML.',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    keywords=['web html markup'],
+    platforms='linux',
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6'
+    ]
+)
