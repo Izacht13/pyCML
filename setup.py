e311a77e245700e5a305e01b25612470edd819da
Isaac Torbett <izacht13@gmail.com>
2017-02-05 20:45:14 -0500

    Still not sure what I'm doing with this whole PIP thing...

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-from setuptools import setup, find_packages
+"""
+pyCasual
+"""
+
+from setuptools import setup
 
 setup(
-    name='pycasual',
+    name="pycasual",
     version="0.0.1",
-    url='http://github.com/izacht13/pyCasual/',
-    license='MIT',
-    author='Isaac Torbett',
-    author_email='izacht13@gmail.com',
-    description='An interpreter for Casual Markup Language.',
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    keywords=['web html markup'],
-    platforms='linux',
-    install_requires=[],
-    entry_points={},
+    url="http://github.com/izacht13/pyCasual/",
+    license="MIT",
+    author="Isaac Torbett",
+    author_email="izacht13@gmail.com",
+    description="An interpreter for Casual Markup Language.",
+    packages=["pycasual"],
+    keywords=["web html markup"],
+    platforms="any",
     classifiers=[
-        'Development Status :: 1 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'License :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6'
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6"
     ]
 )
