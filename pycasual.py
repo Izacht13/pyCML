5388eaf1e73e3e1006f24388b660c993a2d32b72
Isaac Torbett <izacht13@gmail.com>
2017-02-12 22:17:44 -0500

    Lots of changes, almost to many to recall - Rewrote a lot or parsing code - Added pluggable/modular serialization - Temporarily removed tag functions - Added file imports - Improved HTML output - Added Exceptions - Greatly simplified the tree produced by parsing

--- pycasual.py
+++ pycasual.py
@@ -1,14 +1,37 @@
 import re
+import os
 
 
 ##############################
 #/                          \#
-#|      Debug Options       |#
+#|  Serialization Outputs   |#
 #\                          /#
 ##############################
 
-# Flatten nested arrays in tags and content
-ALLOW_TAG_FLATTENING = True
+
+def __soutf_html_before__(source, data):
+	data["tag"] = ''.join(source.tag).strip() or "div"
+	if data["tag"][-1] == '/':
+		data["tag"] = data["tag"][:-1]
+		data["single"] = True
+	else:
+		data["single"] = False
+
+
+SERIALIZATION_OUTPUTS = {
+	"html": {
+		"before": __soutf_html_before__,
+		"content_text": lambda s, d: ''.join(s) if isinstance(s, list) else s,
+		"content": lambda s, d: ''.join(s),
+		"attribute": lambda s, d: "%s=\"%s\"" % s,
+		"attributes": lambda s, d: ''.join(s) if s else '',
+		"child": lambda s, d: s._serialize(SERIALIZATION_OUTPUTS["html"]),
+		"children": lambda s, d: ''.join(s),
+		"element": lambda s, d: (
+			"<{tag}{attributes}>" + ("{content}{children}</{tag}>" if not d["single"] else '')
+		).format(**d)
+	}
+}
 
 
 ##############################
@@ -17,29 +40,9 @@ ALLOW_TAG_FLATTENING = True
 #\                          /#
 ##############################
 
-def __tagfunc_append__(element, args):
-	while args and element.content:
-		element.child(args.pop(), element.content.pop())
 
-
-def __tagfunc_riter__(element, args):
-	if not (element.parent and args):
-		return
-	for child in element:
-		l = len(child.content)
-		attributes = [Attribute(args[0], child.tag)]
-		for i, tag in enumerate(args[1:]):
-			attributes.append(Attribute(tag, child.content[
-							  i] if i < l and child.content != ' ' else None))
-		element.parent.add_child(element.tag, attributes=attributes)
-	element.parent.children.remove(element)
-
-TAG_FUNCTIONS = {
-	"#": "id",
-	".": "class",
-	"<": __tagfunc_riter__,
-	"&": __tagfunc_append__
-}
+def __tfunc_shortcut__(element, tag, args):
+	pass
 
 
 ##############################
@@ -48,72 +51,17 @@ TAG_FUNCTIONS = {
 #\                          /#
 ##############################
 
-class Tag(list):
 
-	def serialize(self):
-		return ''.join([''.join(t) if isinstance(t, list) and ALLOW_TAG_FLATTENING else str(t) for t in self])
-
-
-class Content(Tag):
-	pass
-
-
-class TaggedContent(object):
-
-	def __init__(self, tag=None, content=None):
-		self.tag = Tag(tag or [])
-		self.content = Content(content or [])
-
-	def __eq__(self, x):
-		if isinstance(x, TaggedContent):
-			return self.tag == x.tag
-		return self.tag == x
-
-	def __str__(self):
-		return self.serialize()
-
-	def serialize(self):
-		return "<{tag}>{content}</{tag}>".format(
-			tag=self.tag.serialize().strip() or "div",
-			content=self.content.serialize()
-		)
-
-
-class Attribute(TaggedContent):
-
-	def serialize(self):
-		return self.tag.serialize() + ("=\"%s\"" % self.content.serialize() if self.content else '')
-
-
-class Element(TaggedContent):
+class Element(object):
 
 	def __init__(self, tag=None, parent=None, content=None, attributes=None, children=None):
-		super().__init__(tag, content)
+		self.tag = tag or []
+		self.content = content or []
 		self.children = children or []
-		self.attributes = attributes or []
-
-		if parent and isinstance(parent, Element):
-			self.parent = parent
-		else:
-			self.parent = None
-
-	def __getitem__(self, key):
-		for child in self.children:
-			if child == key:
-				return child
-		raise KeyError
-
-	def __setitem__(self, key, value):
-		child = self.__getitem__(key)
-		child = value
+		self.attributes = attributes or {}
+		self.parent = parent
 
 	def get_child(self, tag):
-		for child in self.children:
-			if child == tag:
-				return child
-		return None
-
-	def get_attribute(self, tag):
 		for child in self.children:
 			if child == tag:
 				return child
@@ -123,19 +71,16 @@ class Element(TaggedContent):
 		self.children.append(Element(tag, self, content, attributes, children))
 		return self.children[-1]
 
-	def add_attribute(self, tag, content=None):
-		self.attributes.append(Attribute(tag, content))
-		return self.attributes[-1]
+	def __getitem__(self, key):
+		return self.attributes[''.join(key) if isinstance(key, list) else key]
 
-	def add(self, item):
-		if isinstance(item, Element):
-			item.parent = self
-			self.children.append(item)
-			return self.children[-1]
-		elif isinstance(item, Attribute):
-			self.attributes.append(item)
-			return self.attributes[-1]
-		return None
+	def __setitem__(self, key, value):
+		self.attributes[''.join(key) if isinstance(key, list) else key] = value
+
+	def __eq__(self, x):
+		if isinstance(x, Element):
+			return self.tag == x.tag
+		return self.tag == x
 
 	def __iter__(self):
 		yield from self.children
@@ -143,47 +88,72 @@ class Element(TaggedContent):
 	def __str__(self):
 		return self.serialize()
 
-	def serialize(self):
-		tag = self.tag.serialize().strip()
-		single = False
-		if tag and tag[-1] == '/':
-			single = True
-			tag = tag[:-1]
-		return ("<{tag} {attributes}>" + ("{content}{children}</{tag}>" if not single else '')).format(
-			tag=tag or "div",
-			content=self.content.serialize(),
-			attributes=''.join([a.serialize() for a in self.attributes]),
-			children=''.join([c.serialize() for c in self.children]),
-		)
+	def __getattr__(self, key):
+		if key in SERIALIZATION_OUTPUTS:
+			return self.serialize(key)
+		else: raise AttributeError
 
-	def transform(self, functions=TAG_FUNCTIONS):
-		e = 0
-		for i, t in enumerate(self.tag):
-			if t in functions:
-				function = functions[t]
-				args = [a for a in self.tag[i + 1:] if a != ' ']
-				self.tag = Tag(self.tag[:i])
-				if callable(function):
-					function(self, args)
-				elif isinstance(function, str):
-					if function[0] == ':':
-						self.add_child(function[1:], args)
-					else:
-						self.add_attribute(function, args)
-				# elif isinstance(function[t], re._pattern_type):
-				# 	for child in self.children:
-				# 		if function[t].search(''.join(child.tag))
-				e += 1
-				break
-		for child in reversed(self.children):
-			e += child.transform(functions)
-		return e
+	def serialize(self, output="html", skiproot=True, *, handlers=None, **kwargs):
+		handlers = handlers or SERIALIZATION_OUTPUTS.get(output)
+		if not handlers: return ''
+		return ''.join([
+			c._serialize(handlers, **kwargs) for c in self.children
+		]) if skiproot else self._serialize(handlers, **kwargs)
+
+	def _serialize(self, handlers, **kwargs):
+		data = kwargs or {}
+		if callable(handlers.get("before")):
+			data.update(handlers["before"](self, data) or {})
+		containers = {
+			"tag": "tag_text",
+			"content": "content_text",
+			"attributes": "attribute",
+			"children": "child"
+		}
+		for container_name, contained_name in containers.items():
+			container_handler = handlers.get(container_name)
+			contained_handler = handlers.get(contained_name)
+			attr = getattr(self, container_name)
+			contained_list = attr.items() if isinstance(attr, dict) else attr
+			items = None
+			if callable(contained_handler):
+				items = [contained_handler(i, data) for i in contained_list]
+			elif isinstance(contained_handler, str):
+				items = [contained_handler.format(source=i, **data) for i in contained_list]
+			new_container = None
+			if callable(container_handler):
+				new_container = container_handler(items, data)
+			elif isinstance(container_handler, str):
+				new_container = contained_handler.format(source=items, **data)
+			data[container_name] = new_container or data.get(container_name) or ''
+		handler = handlers.get("element")
+		if callable(handler):
+			out = handler(self, data)
+		elif isinstance(handler, str):
+			out = handler.format(source=self, **data)
+		else:
+			out = ""
+		if callable(handlers.get("after")):
+			data.update(handlers["after"](self, data) or {})
+
+		return out
 
 
-class Root(Element):
+##############################
+#/                          \#
+#|       Exceptions         |#
+#\                          /#
+##############################
 
-	def serialize(self):
-		return ''.join([c.serialize() for c in self.children])
+
+class ParseError(BaseException):
+	pass
+
+class IndentMismatch(ParseError):
+	pass
+
+class UnknownToken(ParseError):
+	pass
 
 
 ##############################
@@ -196,68 +166,95 @@ class Parser(object):
 
 	class Context(list):
 
-		def push(self, depth, anchor=None):
-			self.append((depth, anchor))
-
-		def isattribute(self, index=None):
-			if isinstance(self[index or -1][1], Attribute):
-				return True
-			return False
-
-		def iselement(self, index=None):
-			if isinstance(self[index or -1][1], Element):
-				return True
-			return False
-
-		def isroot(self, index=None):
-			if isinstance(self[index or -1][1], Root):
-				return True
-			return False
+		def push(self, element, depth=None):
+			self.append((element, depth or (self[-1][1] + 1)))
 
 		def __getattr__(self, name):
-			if name == "top":
+			if name == "element":
+				return self[-1][0]
+			elif name == "parent":
+				return self[-2][0]
+			elif name == "depth":
 				return self[-1][1]
-			super().__getattr__(name)
+			return AttributeError
 
 		def __setattr__(self, name, value):
-			if name == "top":
-				self[-1] = (self[-1][0], value)
+			if name == "element":
+				self[-1] = (value, self[-1][1])
 			else:
 				super().__setattr__(name, value)
 
+	class TokenBuffer(object):
+
+		def __init__(self, stack=None):
+			self.stack = stack or []
+
+		def use(self):
+			s = self.stack
+			self.stack = []
+			return [i[1] for i in s]
+
+		def push(self, token):
+			self.stack.append(token)
+
+		def pop(self):
+			return self.stack.pop()
+
 	class Tokens:
 		UNKNOWN = 0
-		COMMENT = 1
-		RAWSTRING = 2
-		STRING = 3
-		ESCAPE = 4
+		ESCAPE = 1
+		COMMENT = 2
+		RAWSTRING = 3
+		STRING = 4
 		BRACKET = 5
 		ENDBRACKET = 6
 		LINEBREAK = 7
 		BREAK = 8
 		TAG = 9
 		SPACE = 10
-		TEXT = 11
+		WORD = 11
 		SYMBOL = 12
 
+	class States:
+		UNKNOWN = 0
+		TAG = 1
+		ELEMENT = 2
+		ATTRIBUTE = 3
+		TEXT_LIST = 4
+		ATTRIBUTE_LIST = 5
+
+	class Substates:
+		NONE = 0
+		TEST_DEPTH = 1
+		INDENT = 2
+
 	__lexemes__ = [(re.compile(x[1]), x[0]) for x in [
+		(Tokens.ESCAPE, r"\\(.)"),
 		(Tokens.COMMENT, r"\/{2,}\s*(([^\/\n\r])*)(\/{2,})*"),
 		(Tokens.RAWSTRING, r"\"(([^\"\\]|\\.)*)\""),
 		(Tokens.STRING, r"'(([^'\\\n\r]|\\.)*)'"),
-		(Tokens.ESCAPE, r"\\(.)"),
 		(Tokens.BRACKET, r"([\[\{])"),
 		(Tokens.ENDBRACKET, r"([\]\}])"),
 		(Tokens.LINEBREAK, r"([\n\r]+)"),
 		(Tokens.BREAK, r"([,;])[ \t\f\v,;]*"),
 		(Tokens.TAG, r"([:=])[ \t\f\v]*"),
 		(Tokens.SPACE, r"([ \t\f\v])"),
-		(Tokens.TEXT, r"(\w+)"),
+		(Tokens.WORD, r"(\w+)"),
 		(Tokens.SYMBOL, r"([\!\@\#\$\%\^\&\*\?\/\.\+\-\|\~\(\)\`<>])"),
 		(Tokens.UNKNOWN, r"(.)")
 	]]
 
-	def __init__(self, script=None):
+	def __import(self, match):
+		path = match.group(1)
+		regex = match.group(2)
+		if path:
+			with open(os.path.join(self.import_directory, path)) as file:
+				return re.search(regex, file.read()).group(0) if regex else file.read()
+		return ''
+
+	def __init__(self, script=None, import_directory=None):
 		self.script = script
+		self.import_directory = import_directory or '.'
 
 	def tokens(self, script=None):
 		"""Returns a generator of tokens matched in the script."""
@@ -266,141 +263,132 @@ class Parser(object):
 			return
 
 		index = 0
-		scrlen = len(self.script)
+		script_length = len(self.script)
 
 		while True:
 			for lexeme in Parser.__lexemes__:
 				match = lexeme[0].match(self.script[index:])
 				if match:
-					yield (lexeme[1], match.group(1))
 					index += match.span()[1] - 1
+					yield (lexeme[1], match.group(1), index)
 					break
 
-			if index < scrlen:
+			if index < script_length:
 				index += 1
 			else:
 				break
 
-	def parse(self, script=None, transform=True, *, tokens=None):
-		"""Set transform to False to disable tag functions."""
-		self.script = (script or self.script) + ';'
+	def parse(self, script=None, preprocess=True, *, import_directory=None, tokens=None):
+		self.script = script or self.script
+		self.import_directory = import_directory or self.import_directory
 		if not self.script:
 			return
+		else:
+			self.script = re.sub(
+				r"<<[ \t\f\v\.\/\\]*([^>\n\r~]+)~? *([^>\n\r]*)(>>)?"
+				, self.__import, self.script
+			)
+			self.script += ';'
 
 		tokens = tokens or self.tokens()
-		if not tokens:
-			return
+		if not tokens: return
 		token = next(tokens, None)
 
-		root = Root()
-		buffer = []
+		root = Element()
+		buffer = Parser.TokenBuffer()
 		depth = 0
-		depth_test = True
-		indent = False
-		attribute_bracket = False
-		list_bracket = False
-		last_bracket = None
+
+		state = Parser.States.TAG
+		substate = Parser.Substates.TEST_DEPTH
+		state_data = None
+
+		last_break = None
+		last_indent = None
 
 		context = Parser.Context()
-		context.push(-1, root)
+		context.push(root, -1)
 
 		while token:
-			if depth_test:
+			if substate == Parser.Substates.TEST_DEPTH:
 				if token[0] == Parser.Tokens.SPACE:
+					if last_indent and token[1] != last_indent[1]:
+						raise IndentMismatch
 					depth += 1
+					last_indent = token
 				else:
-					depth_test = False
-					if not list_bracket:
-						while depth < context[-1][0]:
+					substate = Parser.Substates.NONE
+					if state != Parser.States.TEXT_LIST:
+						while depth < context.depth:
 							context.pop()
-						if depth > context[-1][0]:
-							indent = True
+						if depth > context.depth:
+							substate = Parser.Substates.INDENT
 					continue
 			else:
-				if token[0] == Parser.Tokens.LINEBREAK:
-					if list_bracket:
-						# This below re-tests for something we already knew...
-						# If the last token was a break or not, but because we already
-						# threw away that data, so we have to re-test.
-						# The only way I can think to fix this right now, is to
-						# include the whole token in the buffer.
-						if buffer and buffer[-1] not in ";,":
-							buffer.append(last_bracket)
-					elif buffer:
-						context.top.content.append(buffer)
-						buffer = []
-					depth = 0
-					depth_test = True
-					indent = False
-				elif token[0] == Parser.Tokens.BREAK:
-					if list_bracket:
-						buffer.append(token[1])
-						last_bracket = token[1]
+				if token[0] in [Parser.Tokens.LINEBREAK, Parser.Tokens.BREAK]:
+					if state == Parser.States.ATTRIBUTE and state_data:
+						context.element[state_data] = buffer.use()
+						state = Parser.States.ELEMENT
+						state_data = None
+
+					if state == Parser.States.TEXT_LIST:
+						if token[0] == Parser.Tokens.LINEBREAK:
+							if buffer[-1][0] != Parser.Tokens.BREAK:
+								buffer.push(last_break)
+						else:
+							buffer.push(token)
+							last_break = token
 					else:
-						if buffer:
-							context.top.content.append(buffer)
-							buffer = []
-						if token[1] == ',' and attribute_bracket:
-							if context.iselement():
-								context.top.attribute(buffer)
-								buffer = []
+						if token[0] == Parser.Tokens.LINEBREAK:
+							depth = 0
+							substate = Parser.Substates.TEST_DEPTH
+						elif token[1] == ',' and state == Parser.States.ATTRIBUTE_LIST:
+							context.element[buffer.use()] = None
+					if buffer:
+						context.element.content.append(buffer.use())
 				elif token[0] == Parser.Tokens.BRACKET:
 					if buffer:
-						context.top.content.append(buffer)
-						buffer = []
-					if token[1] == '[' and not (attribute_bracket or list_bracket):
-						attribute_bracket = True
-					elif token[1] == '{' and not list_bracket:
-						list_bracket = True
-					else:
-						buffer.append(token[1])
-				elif token[0] == Parser.Tokens.ENDBRACKET:
-					if token[1] == ']' and attribute_bracket:
-						if buffer and context.iselement():
-							context.top.add_attribute(buffer)
-							buffer = []
-						attribute_bracket = False
-					elif token[1] == '}' and list_bracket:
-						if buffer:
-							context.top.content.append(buffer)
-							buffer = []
-						list_bracket = False
-						last_bracket = None
-					else:
-						buffer.append(token[1])
-				elif token[0] == Parser.Tokens.TAG:
-					if list_bracket:
-						buffer.append(token[1])
-					else:
-						if token[1] == ':' and not attribute_bracket:
-							new = Element(buffer)
-						elif token[1] == '=':
-							new = Attribute(buffer)
+						context.element.content.append(buffer.use())
+					if state == Parser.States.ELEMENT:
+						if token[1] == '[':
+							states = Parser.States.ATTRIBUTE_LIST
 						else:
-							new = None
-							buffer.append(token[1])
-						if new:
-							if indent and context.iselement():
-								context.top.add(new)
-								context.push(depth, new)
-							elif not indent and context.iselement(-2):
-								context[-2][1].add(new)
-								context.top = new
-							buffer = []
+							states = Parser.States.TEXT_LIST
+					else:
+						buffer.push(token)
+				elif token[0] == Parser.Tokens.ENDBRACKET:
+					if token[1] == ']' and state == Parser.States.ATTRIBUTE_LIST:
+						if buffer:
+							context.element[buffer.use()] = None
+						state = Parser.States.ELEMENT
+					elif state == Parser.States.TEXT_LIST:
+						if buffer:
+							context.element.content.append(buffer.use())
+						state = Parser.States.ELEMENT
+						last_break = None
+				elif token[0] == Parser.Tokens.TAG:
+					if state == Parser.States.TEXT_LIST:
+						buffer.push(token)
+					else:
+						if token[1] == ':' and state != Parser.States.ELEMENT:
+							if substate == Parser.Substates.INDENT:
+								context.push(context.element.add_child(buffer.use()), depth)
+							else:
+								context.element = context.parent.add_child(buffer.use())
+						elif substate == Parser.Substates.INDENT:
+							state_data = buffer.use()
+							context.element[state_data] = None
+							state = Parser.States.ATTRIBUTE
 				elif token[0] == Parser.Tokens.COMMENT:
 					pass
 				elif token[0] == Parser.Tokens.UNKNOWN:
-					pass
+					raise UnknownToken
 				else:
-					buffer.append(token[1])
+					buffer.push(token)
 
 			token = next(tokens, None)
 
-		if transform:
-			root.transform()
 		return root
 
 
-def parse(script=None, transform=True):
-	"""Set transform to False to disable tag functions."""
+def parse(script=None):
 	return Parser(script).parse()
